c5d00be40c1ea00b582a2274d656072a974523a5
quocanlikebanana <quocankg2611@gmail.com>
2025-08-16 15:12:11 +0700

    init

---
+++ src/app/van-ban/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+	return (
+		<div className='bg-egyptian-blue-500 text-goldenrod-900 p-4'>
+			<h1 className='text-2xl font-bold'>Van Ban Page</h1>
+			<p className='mt-2'>This is a sample page using custom Tailwind CSS colors.</p>
+		</div>
+	)
+}
